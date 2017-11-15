51faf601409d89db16e3718657562af777f23d32
Ygytl <ygy.cs06@outlook.com>
2017-11-16 00:04:27 +0800

    X10-5.c

---
+++ hs3/X10-5.c
@@ -0,0 +1,16 @@
+int Ack( int m, int n )
+{
+	if(m==0)
+	{
+		return n+1;
+	}
+	else{
+		if(n==0&&m>0)
+		{
+			return Ack(m-1, 1);
+		}
+		else if(m>0&&n>0){
+			return Ack(m-1, Ack(m, n-1));
+		}
+	}
+}
